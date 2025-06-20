0ffc36d754b53e36406357c3d370d38f700d8303
Abdul Rasheed <rasheed7249@gmail.com>
2025-06-20 22:19:38 +0500

    changed main route

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
+import LoginSystem from './LoginSystem'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <LoginSystem />
   </StrictMode>,
 )
